dd289434c0187ef8f2a6c0d889d82062a9c14720
Alain <carpikemail@gmail.com>
2015-05-09 09:59:31 +0200

    Game class added

---
+++ ingsw/src/main/java/it/polimi/ingsw/server/Game.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.server;
+
+import java.util.List;
+
+public class Game {
+	public boolean addPlayer(Client client) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int getNumberOfPlayers() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
