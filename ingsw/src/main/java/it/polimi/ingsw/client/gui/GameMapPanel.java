5335f606bfad9a7006c036d4e669d0fa52efe9ab
Mitch <michele.albanese@mail.polimi.it>
2015-05-14 09:40:38 +0200

    Add GUI functions and hex generator

--- ingsw/src/main/java/it/polimi/ingsw/client/gui/GameMapPanel.java
+++ ingsw/src/main/java/it/polimi/ingsw/client/gui/GameMapPanel.java
@@ -6,60 +6,142 @@ import it.polimi.ingsw.game.sector.Sectors;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.TexturePaint;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class GameMapPanel extends JPanel {
+	
+	private static final Logger log = Logger.getLogger( GameMapPanel.class.getName() );
+	
 	private GameMap gameMap;
 	
-	// Sector ID <-> image
+	/* ----- to be removed soon -------- */
+	private BufferedImage img, img2; 
+	private Polygon p1, p2;
+	/* ----- to be removed soon -------- */
+	
 	private Map<Integer, BufferedImage> sectorImages;
+	private TexturePaint texture;
+	
+	private Polygon currentPolygon;
+	
+	private final int width = 800;
+	private final int height = 600;
+	private final int widthExternalSpace = 9; // externalspace + width - 2 * externalspace + externalspace
+	private final int heightExternalSpace = 6; // same for height
+	
+	int hexWidth, hexSize, hexHeight;
 	
 	public GameMapPanel( GameMap map ) {
 		super();
 		
 		gameMap = map;
-		readImages();
+				
+		this.addMouseMotionListener( new HexListener() );
 		this.setBackground(Color.WHITE);
+		
+		currentPolygon = null;
+		
+		loadTextures();
+		calcValuesForHexagons();
+		
+		setInitialState();
 	}
 	
 	public Dimension getPreferredSize() {
-	        return new Dimension(GameScreen.WIDTH*5/8, GameScreen.HEIGHT);
+	        return new Dimension(width,height);
+	}
+	
+	private void loadTextures() {
+    	sectorImages = new HashMap<>();
+    	
+    	try {  
+    		/* ----- to be removed soon -------- */
+    		img = ImageIO.read(new File("images/texture.jpg"));
+			img2 = ImageIO.read(new File("images/hex.png"));
+			/* ----- to be removed soon -------- */
+			
+    	    sectorImages.put(Sectors.ALIEN, ImageIO.read(new File("images/sector_alien.png")));
+    	    sectorImages.put(Sectors.DANGEROUS, ImageIO.read(new File("images/sector_dangerous.png")));
+    	    sectorImages.put(Sectors.NOT_DANGEROUS, ImageIO.read(new File("images/sector_not_dangerous.png")));
+    	    sectorImages.put(Sectors.HATCH, ImageIO.read(new File("images/sector_hatch.png")));
+    	    sectorImages.put(Sectors.HUMAN, ImageIO.read(new File("images/sector_human.png")));
+    	    sectorImages.put(Sectors.NOT_VALID, ImageIO.read(new File("images/sector_not_valid.png")));
+    	} catch (IOException e) {
+    		log.log(Level.WARNING, "Texture images for sectors not found.");
+    	}
 	}
 
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);       
+    	super.paintComponent(g);       
 
-        drawMap(g);
+    	Graphics2D g2d = (Graphics2D)g;
+
+    	RenderingHints rh = new RenderingHints(
+    			RenderingHints.KEY_ANTIALIASING,
+    			RenderingHints.VALUE_ANTIALIAS_ON);
+    	g2d.setRenderingHints(rh);
+
+    	TexturePaint slatetp = new TexturePaint(img, new Rectangle(0, 0, 90, 60));
+    	g2d.setPaint(slatetp);
+    	
+    	g2d.fillPolygon( p1 );    
+    	g2d.fillPolygon( p2 );  
+    	
+    	if( currentPolygon != null ) {
+    		TexturePaint slatetp1 = new TexturePaint(img2, new Rectangle(0, 0, 90, 60));
+        	g2d.setPaint(slatetp1);
+    		g2d.fillPolygon( currentPolygon );
+    	}
     }  
     
-    private void drawMap(Graphics g) {
-    	float maxSectorWidth = GameScreen.WIDTH / GameMap.WIDTH; 
-    	float maxSectorHeight = GameScreen.HEIGHT / GameMap.HEIGHT;
-    	
-    	
-    	
+    class HexListener implements MouseMotionListener {
+		@Override
+		public void mouseDragged(MouseEvent arg0) {	}
+
+		@Override
+		public void mouseMoved(MouseEvent arg0) {
+			// FIXME: Polygon p = getPolygonFromPoint();
+			if( p1.contains(arg0.getPoint()) ) {
+				currentPolygon = p1;
+			} else {
+				currentPolygon = null;
+			}
+			
+			repaint();
+		}
     }
     
-    private void readImages() {
-    	/* FIXME: IMAGE HANDLER */
-    	sectorImages = new HashMap<>();
-    	
-    	try {
-    	    sectorImages.put(Sectors.ALIEN, ImageIO.read(new File("sector_alien.png")));
-    	    sectorImages.put(Sectors.DANGEROUS, ImageIO.read(new File("sector_dangerous.png")));
-    	    sectorImages.put(Sectors.NOT_DANGEROUS, ImageIO.read(new File("sector_not_dangerous.png")));
-    	    sectorImages.put(Sectors.HATCH, ImageIO.read(new File("sector_hatch.png")));
-    	    sectorImages.put(Sectors.HUMAN, ImageIO.read(new File("sector_human.png")));
-    	    sectorImages.put(Sectors.NOT_VALID, ImageIO.read(new File("sector_not_valid.png")));
-    	} catch (IOException e) {
-    		
-    	}
+    private int calcHexWidth() {
+    	return (width - 2 * widthExternalSpace)/23; // FIXME: cambia quel 23 con la costante dalla classe Sectors!
     }
+    
+    private void setInitialState() {
+    	p1 = HexagonFactory.createHexagon( new Point(200,200), hexSize );
+		p2 = HexagonFactory.createHexagon( new Point(200,200+hexHeight), hexSize );
+    }
+    
+    /* Calculate how much space we can dedicate to each hexagon. */
+	private void calcValuesForHexagons() {
+		
+		hexWidth = calcHexWidth();
+		hexSize = hexWidth / 2;
+		hexHeight = (int)( Math.sqrt(3) * 2 * hexSize );
+	}
 }
---
+++ ingsw/src/main/java/it/polimi/ingsw/client/gui/HexagonFactory.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.client.gui;
+import java.awt.Point;
+import java.awt.Polygon;
+
+public class HexagonFactory  {
+	public static final int numberOfVertices = 6;
+	
+	private HexagonFactory() {}
+	
+	public static Polygon createHexagon( Point center, int size ) {
+    	int xPts[] = new int[numberOfVertices];
+    	int yPts[] = new int[numberOfVertices];
+    	
+    	// get vertices by simply rotating by PI/6 for 6 times
+    	for( int i = 0; i < numberOfVertices; ++i ) {
+    		xPts[i] = (int)(center.getX() + size * Math.cos(i*Math.PI/3));
+    		yPts[i] = (int)(center.getY() + size * Math.sin(i*Math.PI/3));
+    	}
+    	
+    	return new Polygon( xPts, yPts, numberOfVertices );
+    }
+	
+}
