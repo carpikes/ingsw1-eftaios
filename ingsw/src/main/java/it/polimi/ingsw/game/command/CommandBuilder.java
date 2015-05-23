2a06f6e909bf631acbf7ae144164f90d42e18610
Mitch <michele.albanese@mail.polimi.it>
2015-05-23 21:43:56 +0200

    Add CommandBuilder (serverside)

--- ingsw/src/main/java/it/polimi/ingsw/game/command/CommandBuilder.java
+++ ingsw/src/main/java/it/polimi/ingsw/game/command/CommandBuilder.java
@@ -3,8 +3,12 @@
  */
 package it.polimi.ingsw.game.command;
 
+import it.polimi.ingsw.exception.NetworkPacketNotValidException;
+import it.polimi.ingsw.game.card.ObjectCard;
 import it.polimi.ingsw.game.network.GameCommands;
 import it.polimi.ingsw.game.network.NetworkPacket;
+
+import java.awt.Point;
 
 /**
  * @author Michele
@@ -16,14 +20,14 @@ public class CommandBuilder {
      * @param opcode
      * @return
      */
-    public static Command getCommandFromNetworkPacket(NetworkPacket packet) {
+    public static Command getCommandFromNetworkPacket(NetworkPacket packet) throws ClassCastException {
         switch( packet.getOpcode() ) {
-        case GameCommands.CMD_CS_ATTACK:    
-            
-            
+            case GameCommands.CMD_CS_ATTACK:            return new AttackCommand( (Point) packet.getArgs()[0] );
+            case GameCommands.CMD_CS_DISCARD_OBJ_CARD:  return new DiscardObjectCard( (int) packet.getArgs()[0] );
+            case GameCommands.CMD_CS_USE_OBJ_CARD:      return new UseObjectCardCommand( (ObjectCard) packet.getArgs()[0] );
+            case GameCommands.CMD_CS_SET_POSITION:      return new AttackCommand( (Point) packet.getArgs()[0] );
+            default:                                    throw new NetworkPacketNotValidException("Network packet not valid. Discarded.");                  
         }
-        
-        return null;
     }
     
 }
