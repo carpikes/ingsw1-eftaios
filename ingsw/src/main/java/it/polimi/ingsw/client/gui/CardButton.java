336f4c851aa6a3f54d3374b4e275099765d04679
Mitch <michele.albanese@mail.polimi.it>
2015-06-07 10:21:21 +0200

    Add hovering on cardbuttons && add action buttons on GUI

--- ingsw/src/main/java/it/polimi/ingsw/client/gui/CardButton.java
+++ ingsw/src/main/java/it/polimi/ingsw/client/gui/CardButton.java
@@ -4,8 +4,11 @@
 package it.polimi.ingsw.client.gui;
 import it.polimi.ingsw.client.GameController;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.AlphaComposite;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -15,6 +18,7 @@ import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.SwingUtilities;
 
 /**
  * @author Michele
@@ -27,19 +31,36 @@ public class CardButton extends JButton {
     private int id;
     private BufferedImage image;
     private GameController controller;
+
+    private float alpha = 1f;
     
     public CardButton( final CardButtons type, GameController c ) {
         super( );
 
         controller = c;
         changeTo( type );
-
-        this.addActionListener( new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                controller.sendChosenObjectCard( type.getId() );
+                
+        this.addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent e) {
+                if( SwingUtilities.isLeftMouseButton(e) && e.getClickCount() == 1) {
+                    controller.sendChosenObjectCard( type.getId() );
+                } else if (SwingUtilities.isRightMouseButton(e) && e.getClickCount() == 1) {
+                   //controller.discardObjectCard( type.getId() );
+                }
             }
 
+            public void mousePressed(MouseEvent e) { }
+
+            public void mouseReleased(MouseEvent e) { }
+
+            public void mouseEntered(MouseEvent e) { 
+                setAlpha(0.5f);
+            }
+
+            public void mouseExited(MouseEvent e) { 
+                setAlpha(1f);
+                
+            }
         });
     }
 
@@ -64,5 +85,21 @@ public class CardButton extends JButton {
         this.id = id;
     }
     
+    public float getAlpha()
+    {
+      return alpha;
+    }
+
+    public void setAlpha(float alpha)
+    {
+      this.alpha = alpha;
+      repaint();
+    }
     
+    public void paintComponent(Graphics g)
+    {
+      Graphics2D g2 = (Graphics2D) g;
+      g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+      super.paintComponent(g2);
+    }
 }
--- ingsw/src/main/java/it/polimi/ingsw/client/gui/GUIFrame.java
+++ ingsw/src/main/java/it/polimi/ingsw/client/gui/GUIFrame.java
@@ -7,10 +7,11 @@ import it.polimi.ingsw.game.config.Config;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Set;
 import java.util.logging.Level;
@@ -21,6 +22,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
 
 /**
@@ -31,93 +33,128 @@ import javax.swing.border.EmptyBorder;
 
 public class GUIFrame extends JFrame {
     private static final long serialVersionUID = 1L;
-    
+
     private static final Logger LOG = Logger.getLogger( GUIFrame.class.getName() );
-    
+
+    private final GameController mController;
+
     // Constants
     private static final int WIDTH = 1024;
-    private static final int WIDTH_LEFT  = 770;
+    private static final int WIDTH_LEFT  = 745;
     private static final int WIDTH_RIGHT  = WIDTH - WIDTH_LEFT;
     private static final int HEIGHT = 768;
-    
+
     private static final int PANEL_MARGIN = 10;
     private static EmptyBorder RIGHT_PANEL_MARGIN = new EmptyBorder(PANEL_MARGIN, PANEL_MARGIN, PANEL_MARGIN, PANEL_MARGIN);
     private static final int CARD_HGAP = 5;
     private static final int CARD_VGAP = 5;
-    
+
     private static final int CARD_WIDTH = WIDTH_RIGHT - 2 * PANEL_MARGIN - 2 * CARD_HGAP;
     private static final int CARD_HEIGHT = CARD_WIDTH * 745 / 490; // values based on image size
-    
+
     private static final Insets TEXT_AREA_PADDING = new Insets(5, 5, 5, 5);
     private static final EmptyBorder TEXT_AREA_MARGIN = new EmptyBorder( 10, 0, 10, 0 );
-    
+
     private static final Dimension mDimensionLeftPanel = new Dimension(WIDTH_LEFT, HEIGHT);
     private static final Dimension mDimensionRightPanel = new Dimension(WIDTH_RIGHT, HEIGHT);
-    private static final Dimension mDimensionCardPanel = new Dimension(CARD_WIDTH, CARD_HEIGHT);
-    
-    private final int numberOfButtons = Config.MAX_NUMBER_OF_OBJ_CARDS + 1;
-    private CardButton[] cardButtons = new CardButton[ numberOfButtons ];
-    
-    // Drawing canvas
+    private static final Dimension mDimensionCardPanel = new Dimension(WIDTH_RIGHT, CARD_HEIGHT);
+
+    // Drawing canvas on the left
     private MapCanvasPanel mMapCanvas;
     private LoginCanvasPanel mLoginCanvas;
-    private final GameController mController;
-    
+
+    // elements on right panel
+    private final int numberOfCardButtons = Config.MAX_NUMBER_OF_OBJ_CARDS + 1;
+    private CardButton[] cardButtons = new CardButton[ numberOfCardButtons ];
+
+    JPanel rightPanel, cardPanel, actionButtonsPanel;
+    JScrollPane scrollTextAreaPane;
+
+    JTextArea textArea;
+    JButton btnAttack, btnDrawDangerousCard, btnEndTurn;
+
+
     public GUIFrame(GameController controller) {
         super();
-        
+
         mController = controller;
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);   
         setTitle("Escape from the Aliens in Outer Space");
         setSize(WIDTH, HEIGHT);
         setLocationRelativeTo(null);
         setResizable(false);
-        
+
         createRightPanel();
         switchToLogin();
-       
     }
-    
+
     /**
      * 
      */
     private void createRightPanel() {
         // general settings
-        JPanel rightPanel = new JPanel(  );
+        rightPanel = new JPanel(  );
 
+        rightPanel.setPreferredSize( mDimensionRightPanel );
         rightPanel.setBorder( RIGHT_PANEL_MARGIN );
-        rightPanel.setPreferredSize(mDimensionRightPanel);
         rightPanel.setLayout( new BorderLayout() );
-        
+
         // card panel
-        JPanel cardPanel = createCardPanel(rightPanel);
+        cardPanel = createCardPanel(rightPanel);
         rightPanel.add(cardPanel, BorderLayout.NORTH);
-        
+
         // Message Area
-        JScrollPane scrollPane = createMessageArea();
-        rightPanel.add(scrollPane, BorderLayout.CENTER);
-        
+        scrollTextAreaPane = createMessageArea();
+        rightPanel.add(scrollTextAreaPane, BorderLayout.CENTER);
+
         // Action buttons panel 
-        JPanel actionButtonsPanel = createActionPanel();
+        actionButtonsPanel = createActionPanel();
         rightPanel.add(actionButtonsPanel, BorderLayout.SOUTH);
-        
+
         // add all this to parent's frame
         this.add(rightPanel, BorderLayout.EAST);
     }
 
     private JPanel createActionPanel() {
-        JPanel actionButtonsPanel = new JPanel( new FlowLayout() );
-        
-        JButton btnAttack = new JButton("Attack");
-        JButton btnDiscardCard = new JButton("Discard Card");
-        
-        actionButtonsPanel.add(btnAttack);
-        actionButtonsPanel.add(btnDiscardCard);
+        JPanel actionButtonsPanel = new JPanel( new GridLayout(2,2) );
+
+        btnAttack = new JButton("Attack");
+        btnAttack.addActionListener( new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mController.attack();
+            }
+        });
+
+        btnDrawDangerousCard = new JButton("Dangerous Card");
+        btnDrawDangerousCard.addActionListener( new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mController.drawDangerousCard();
+            }
+        });
+
+        btnEndTurn = new JButton("End Turn");
+        btnEndTurn.addActionListener( new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mController.endTurn();
+            }
+        });
+
+        actionButtonsPanel.add( btnAttack );
+        actionButtonsPanel.add( btnDrawDangerousCard );
+        actionButtonsPanel.add( btnEndTurn );
+
         return actionButtonsPanel;
     }
 
     private JScrollPane createMessageArea() {
-        JTextArea textArea = new JTextArea( 5, 20 );
+        textArea = new JTextArea( 5, 20 );
         textArea.setText("Player 1 has just done nothing because this is just a test.\n\n");
         textArea.append("Player 2 has just done nothing because this is just a test too.\n\n");
         textArea.setLineWrap(true);
@@ -128,20 +165,20 @@ public class GUIFrame extends JFrame {
         JScrollPane scrollPane = new JScrollPane(textArea); 
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setBorder( TEXT_AREA_MARGIN );
-        
+
         return scrollPane;
     }
 
     private JPanel createCardPanel(JPanel rightPanel) {
         GridLayout layout = new GridLayout(0, 2);
+
         layout.setHgap( CARD_HGAP );
         layout.setVgap( CARD_VGAP );
 
         JPanel cardPanel = new JPanel( layout ); // set 2 card per line, "unlimited" number of rows
+        cardPanel.setPreferredSize(mDimensionCardPanel);
 
-        rightPanel.setPreferredSize(mDimensionCardPanel);
-
-        for( int i = 0; i < numberOfButtons; ++i ) {
+        for( int i = 0; i < numberOfCardButtons; ++i ) {
             cardButtons[i] = new CardButton( CardButtons.NULL, mController );
             cardPanel.add( cardButtons[i] );
         }
@@ -150,7 +187,7 @@ public class GUIFrame extends JFrame {
 
         return cardPanel;
     }
-    
+
     public void setRemainingTime(int remainingTime) {
         if(mLoginCanvas != null)
             mLoginCanvas.setTime(remainingTime);
@@ -159,14 +196,14 @@ public class GUIFrame extends JFrame {
         if(mLoginCanvas != null)
             mLoginCanvas.setPlayers(newPlayers);
     }
-    
+
     private void switchToLogin() {
         mLoginCanvas = new LoginCanvasPanel( );
         mLoginCanvas.setPreferredSize(mDimensionLeftPanel);
-       
-        add(mLoginCanvas, BorderLayout.WEST);
+
+        add(mLoginCanvas, BorderLayout.CENTER);
     }
-    
+
     public void switchToMap(GameMap map, Point startingPoint) {
         if(mLoginCanvas == null)
             throw new RuntimeException("Map is already loaded");
@@ -177,11 +214,11 @@ public class GUIFrame extends JFrame {
             LOG.log(Level.SEVERE, "File is not well formatted: " + e);
             System.exit(1);
         }
-        
+
         remove(mLoginCanvas);
-        add(mMapCanvas, BorderLayout.WEST);
+        add(mMapCanvas, BorderLayout.CENTER);
         mLoginCanvas = null;
-        
+
     }
 
     public Point getChosenMapCell() {
@@ -199,14 +236,26 @@ public class GUIFrame extends JFrame {
     public void setPlayerPosition(Point startingPoint) {
         mMapCanvas.setPlayerPosition(startingPoint);
     }
-    
-    public static void main(String[] args) throws IOException, InterruptedException {
-        GUIFrame f = new GUIFrame(null);
-        GameMap map = GameMap.createFromId(1);
-        f.switchToMap(map, map.getStartingPoint(true));
-        f.setVisible(true);
-        
-        
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                GUIFrame f = new GUIFrame( new GameController() );
+                GameMap map = null;
+                try {
+                    map = GameMap.createFromId(1);
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                f.switchToMap(map, map.getStartingPoint(true));
+                f.setVisible(true);
+            }
+        });
     }
-    
+
 }
+
