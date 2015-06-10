7417c0832657c73b21d8e73644f55e3627cb43de
Mitch <michele.albanese@mail.polimi.it>
2015-06-10 13:34:49 +0200

    Add color palette

---
+++ ingsw/src/main/java/it/polimi/ingsw/client/gui/ColorPalette.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.client.gui;
+
+import java.awt.Color;
+
+public class ColorPalette {
+    public static final Color PLAYER_ON                = Color.YELLOW;
+    public static final Color MOUSE_ON_THIS            = Color.CYAN;
+    public static final Color ALIEN                    = new Color(0,50,0);
+    public static final Color DANGEROUS                = new Color(150,150,150);
+    public static final Color NOT_DANGEROUS            = new Color(255,255,255);
+    public static final Color HATCH                    = new Color(47,53,87);
+    public static final Color USED_HATCH               = new Color(178,0,0);
+    public static final Color HUMAN                    = new Color(50,0,0);
+    public static final Color STROKE                   = Color.DARK_GRAY;
+    public static final Color NOISE                    = Color.GREEN;
+    public static final Color NOT_VALID                = null;
+}
